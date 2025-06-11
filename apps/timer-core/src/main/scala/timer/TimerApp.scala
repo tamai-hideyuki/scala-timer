224083f787bb73865503074bf9d71bcf82b1549e
Hideyuki-T <hide27yuki@gmail.com>
2025-06-11 09:14:16 +0900

    [fix]App を使わず main メソッドで明示的に書く

--- apps/timer-core/src/main/scala/timer/TimerApp.scala
+++ apps/timer-core/src/main/scala/timer/TimerApp.scala
@@ -1,13 +1,20 @@
 package timer
 
-object TimerApp extends App {
-  val minutes: Int = if (args.length > 0) args(0).toIntOption.getOrElse(1) else 1
-  println(s"‍♂️ Ritual begins: $minutes minutes")
+object TimerApp {
+  def main(args: Array[String]): Unit = {
+    val minutes: Int =
+      if (args.length > 0) args(0).toIntOption.getOrElse(1)
+      else 1
 
-  for (i <- minutes to 1 by -1) {
-    println(s" $i minute(s) remaining...")
-    Thread.sleep(60 * 1000)
+    println(s"‍♂️ Ritual begins: $minutes minutes")
+
+    for (i <- minutes to 1 by -1) {
+      println(s" $i minute(s) remaining...")
+      Thread.sleep(60 * 1000)
+    }
+
+    println(" Time's up! End of ritual.")
   }
-
-  println(" Time's up! End of ritual.")
 }
+
+
